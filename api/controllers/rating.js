92994030472bc9a4520ecdf07658fd2a08cd8cda
Noor Rizwan <noor.rizwan@microfocus.com>
2021-08-28 14:32:54 +0530

    Insert or Update rating of book

---
+++ api/controllers/rating.js
@@ -0,0 +1,50 @@
+const db = require('../config/dbconfig');
+
+const getRating = (request, response) => {
+    const ratingId = request.params['ratingId'];
+    let query = "";
+    if (ratingId) {
+        console.log('querying rating with rating_id' + ratingId);
+        query = 'select * from books.ratings rating where rating.rating_id=\'' + ratingId + '\'';
+    } else {
+        const bookId = request.params['bookId'];
+        const userId = request.params['userId'];
+        query = 'select * from books.ratings rating where rating.book_id=\'' + bookId + '\' and rating.user_id=\'' + userId + '\'';
+
+    }
+    console.log(`rating query: ${query}`);
+    db.query(query, (err, res) => {
+        if (err) {
+            response.status(500).json(err);
+        } else {
+            response.status(res.statusCode).json(res.data);
+        }
+    });
+}
+
+const createRating = (request, response) => {
+
+    db.upsert({
+        table: 'ratings',
+        records: [
+            {
+                user_id: request.body.userId,
+                book_id: request.body.bookId,
+                rating: request.body.rating,
+                rating_id: request.body.ratingId
+            }
+        ]
+    }, (err, res) => {
+        console.log(`error: ${err}`);
+        console.log(`res: ${res}`);
+        if (err) {
+            response.status(500).json(err);
+        }
+        response.status(200).json(res.data);
+    });
+}
+
+module.exports = {
+    createRating,
+    getRating
+}
