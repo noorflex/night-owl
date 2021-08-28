84702561f55f600c78826065a64dedb86653f105
Noor Rizwan <noor.rizwan@microfocus.com>
2021-08-28 22:04:07 +0530

    Display Error for invalid login

--- ui-app/src/components/login/Login.js
+++ ui-app/src/components/login/Login.js
@@ -7,6 +7,7 @@ import { useHistory } from "react-router";
 const Login = ({ setToken }) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const history = useHistory();
     const validateForm = () => {
         return username.length > 0 && password.length > 0;
@@ -29,27 +30,34 @@ const Login = ({ setToken }) => {
             password
         });
         setToken = token[0];
-        setLoggedIn(token[0]);
-        setLoggedInUser(token[0].user_id, token[0].username);
-        history.push("/books");
+        if (setToken) {
+            setLoggedIn(token[0]);
+            setLoggedInUser(token[0].user_id, token[0].username);
+            history.push("/books");
+        } else {
+            setError("Invalid username or password")
+        }
     }
 
     return (
-        <div className="login-form">
-            <h1>Please Log In</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Username</p>
-                    <input type="text" className="form-field" onChange={e => setUsername(e.target.value)} />
-                </label>
-                <label>
-                    <p>Password</p>
-                    <input type="password" className="form-field" onChange={e => setPassword(e.target.value)} />
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
+        <div className="login-form-wrapper">
+            <div className="login-form">
+                <h2>Please Log In</h2>
+                <div className="error">{error}</div>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <p>Username</p>
+                        <input type="text" className="form-field" onChange={e => setUsername(e.target.value)} />
+                    </label>
+                    <label>
+                        <p>Password</p>
+                        <input type="password" className="form-field" onChange={e => setPassword(e.target.value)} />
+                    </label>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
