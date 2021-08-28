12d2ff97204eafdbb7618dda1fde4612e199de3a
Noor Rizwan <noor.rizwan@microfocus.com>
2021-08-28 14:32:41 +0530

    Return user details

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,11 @@
+const db = require('../config/dbconfig');
+
 const login = (request, response) => {
-    response.send({
-        token: "123456"
-    });
+    db.query('select user_id, username from books.users user where user.username=\'' + request.body['username'] + '\'',
+        (err, res) => {
+            if (err) response.status(500).json(err);
+            response.status(200).json(res.data);
+        });
 };
 
 module.exports = {
