4f875fa6fafe84ece676fc7978b975893cec368b
Noor Rizwan <noor.rizwan@microfocus.com>
2021-08-30 22:30:15 +0530

    Add Bootstrap

--- ui-app/src/App.js
+++ ui-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AppContainer from './components/AppContainer';
 
 function App() {
   return (
-    <AppContainer/>
+    <AppContainer />
   );
 }
 
